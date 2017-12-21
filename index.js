61b21df1b01a467b200436eaedda39140aad2a92
Eric Zivnuska <ezivnuska@gmail.com>
2017-12-20 23:20:33 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var querystring = require('querystring');
+var http1 = require("http");
+var request = require("request");
+var RiveScript = require('rivescript');
+var bot = new RiveScript();
+
+bot.loadDirectory("brain");
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    io.emit('chat message', msg);
+    bot.sortReplies();
+
+    // And now we're free to get a reply from the brain!
+    var reply = bot.reply("local-user", msg);
+    io.emit('chat message', reply);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
